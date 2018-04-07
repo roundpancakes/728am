219096716174a2925174d93cd18adb4fa7d775e9
Petr Vakoč <petr@Petr-MacBook-Air.local>
2018-04-07 11:20:42 +0200

    sdfghkjll

--- index.vr.js
+++ index.vr.js
@@ -5,27 +5,24 @@ import {
   Pano,
   Text,
   View,
+  Video
 } from 'react-vr';
 
 export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
+        <Pano source={asset('car5.jpg')}/>
+        {/* <Video
+          style={{width: 3.0,height: 2.0,
             layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+            transform: [{translate: [0, 0, -2]}],}}
+
+          source= {asset('ivy.mp4')}
+          >
+        </Video> */}
       </View>
     );
   }
